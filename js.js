900ffe20ea06109f8ddc9faf5a225f96c918272b
ichaanc <christiankylecapistani@gmail.com>
2023-09-25 13:14:18 +0800

    Activity_12

---
+++ js.js
@@ -0,0 +1,39 @@
+let todoInput = document.querySelector('#todoInput');
+let taskList = document.querySelector('#taskList');
+let addTask = document.querySelector('#addTask');
+
+   
+   addTask.addEventListener("click", function () {
+  if (todoInput.value != ""){
+      let li = document.createElement("li");
+      let button = document.createElement("button");
+
+     
+      let inputValue = document.createTextNode(todoInput.value);
+      let buttonText = document.createTextNode("x");
+      
+    
+      li.appendChild(inputValue);
+      button.appendChild(buttonText);
+      
+    
+      taskList.appendChild(li).appendChild(button);
+      
+   
+      li.addEventListener("click", function (event) {
+        const target = event.target;
+        target.classList.toggle("done");
+      });
+
+    
+      button.addEventListener("click", function (event) {
+        const target = event.currentTarget;
+        target.closest("li").remove();
+        alert("appendChildRemove");
+      });
+
+    } else {
+        alert("please write something")
+    }
+    }
+  );
